16a9e45e1a298680ea6a90237dd79b128755dc0b
firo33 <133701536+firo33@users.noreply.github.com>
2023-05-15 23:13:16 +0400

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+})
+
+var day_night = document.querySelector(".toggle");
+const body = document.querySelector(".body");
+var header = document.querySelector("header")
+
+day_night.addEventListener("click", () => {
+  header.classList.toggle("day");
+  body.classList.toggle("day");
+})
+
+const readMoreButton = document.querySelector('.btn-read');
+const servicesSection = document.querySelector('#serv');
+
+readMoreButton.addEventListener('click', () => {
+  servicesSection.scrollIntoView({ behavior: 'smooth' });
+});
